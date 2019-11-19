d426621ab22f24469336284e2dbcd83c3cb4c744
Zachary Jacobs <skatboyza124@hotmail.com>
2019-11-19 04:40:12 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,7 +8,7 @@ end
   def introduction_with_language(name,language)
     puts "Hi, my name is #{name} and I am learning to program in #{language}."
   end
-  def introduction_with_language_optional(name,language)
+  def introduction_with_language_optional(name,language="Ruby")
      puts "Hi, my name is #{name} and I am learning to program in #{language}."
   end
 
